b73ac4ed65ebfb8e24de531153a38174d9795423
Raja Nakka <raja.sanjeevkumar@gmail.com>
2013-07-11 16:30:29 +0000

    update web.js

--- web.js
+++ web.js
@@ -3,9 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //var k = fs.readFileSync('./index.html');
+    //response.send(k.toString('utf-8'));
+    response.send('dadfasfas');
+//     response.end();
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
