d3328edce4bd3c1d93f75d1888b38159b1480b89
Raja Nakka <raja.sanjeevkumar@gmail.com>
2013-07-11 16:34:22 +0000

    update web.js

--- web.js
+++ web.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //var k = fs.readFileSync('./index.html');
-    //response.send(k.toString('utf-8'));
-    response.send('dadfasfas');
+  var k = fs.readFileSync('./index.html');
+  response.send(k.toString('utf-8'));
+//    response.send('dadfasfas');
 //     response.end();
 });
 var port = process.env.PORT || 5000;
